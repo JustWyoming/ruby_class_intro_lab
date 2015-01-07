70fff2a3a9e4db5aa54505b4c73c3a0a1b8ec015
JustWyoming <wyomingt@gmail.com>
2015-01-07 09:45:14 -0800

    updated starship to change from class variable to instance

--- starship.rb
+++ starship.rb
@@ -4,28 +4,30 @@
   
 #   * Give your `Starship` an `accelerateTo` method that sets `currentSpeed` to some number, unless that number is greater than its `topSpeed`.
 
+# do NOT use top speed as class variable, because it will change for ALL new ships. So it needs to be an instance variable
+
 class Starship
 
   attr_accessor :model, :owner, :current_speed
-  	@@top_speed = 200
+  
 	def initialize model, owner_name
 		@model = model
 		@owner = owner_name
+		@top_speed = 200
 		
 	end
 
 	def top_speed
-		@@top_speed
+		@top_speed
 	end
 
-	def set_top_speed faster_speed
-		@faster_speed = faster_speed
-		@@top_speed = @faster_speed
+	def set_top_speed faster
+		@top_speed = faster
 	end
 
 	def accelerate_to increase
 		@increase = increase
-		if (@increase < @@top_speed)
+		if (@increase < @top_speed)
 			@current_speed = @increase
 			@current_speed
 		else
